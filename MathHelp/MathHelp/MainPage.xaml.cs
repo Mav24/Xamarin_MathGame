ad35a189cde6230897887d324deb56bf9a385ce4
Maverick <murrayduke@rogers.com>
2019-02-18 20:39:12 -0500

    Basic UI finished, math equations working, need more functions.

--- MathHelp/MathHelp/MainPage.xaml.cs
+++ MathHelp/MathHelp/MainPage.xaml.cs
@@ -9,9 +9,44 @@ namespace MathHelp
 {
     public partial class MainPage : ContentPage
     {
+        int firstNumber = 0;
+        int secondNumber = 0;
         public MainPage()
         {
             InitializeComponent();
+            RandomNumber();
+        }
+
+        private void Submit_Clicked(object sender, EventArgs e)
+        {
+            var total = firstNumber * secondNumber;
+            var userAnswer = int.Parse(answer.Text);
+            if (userAnswer == total)
+            {
+                result.TextColor = Color.Green;
+                result.Text = "Correct!";
+                RandomNumber();
+                answer.Text = "";
+                answer.Focus();
+            }
+            else
+            {
+                result.TextColor = Color.Red;
+                result.Text = "Sorry try again!";
+                answer.Text = "";
+                answer.Focus();
+            }
+
+        }
+
+        private void RandomNumber()
+        {
+
+            Random num = new Random();
+            firstNumber = num.Next(2, 10);
+            secondNumber = num.Next(2, 10);
+
+            question.Text = $"{firstNumber} " + "X" + $" {secondNumber}".ToString();
         }
     }
 }

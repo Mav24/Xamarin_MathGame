c9f156e1b66f23d5dda66910614c3b6fd9a4bec1
Maverick <murrayduke@rogers.com>
2019-02-20 21:41:33 -0500

    Added Main page, cleaned up code on multiplication page

--- MathHelp/MathHelp/MainPage.xaml.cs
+++ MathHelp/MathHelp/MainPage.xaml.cs
@@ -9,54 +9,11 @@ namespace MathHelp
 {
     public partial class MainPage : ContentPage
     {
-        int firstNumber = 0;
-        int secondNumber = 0;
-        int rightAnswer = 0;
-        int wrongAnswer = 0;
-        
-
         public MainPage()
         {
             InitializeComponent();
-            RandomNumber();
-            
         }
 
-        private void Submit_Clicked(object sender, EventArgs e)
-        {
-            var total = firstNumber * secondNumber;
-            if(!int.TryParse(answer.Text , out int userAnswer))
-            {
-                DisplayAlert("Error", "Numeric values only", "Try Again!");
-            }
-            else if (userAnswer == total)
-            {
-                rightAnswer++;
-                totalRight.TextColor = Color.Green;
-                totalRight.Text = $"Correct: {rightAnswer}";
-                RandomNumber();
-                answer.Text = "";
-                answer.Focus();
-            }
-            else
-            {
-                wrongAnswer++;
-                totalWrong.TextColor = Color.Red;
-                totalWrong.Text = $"Wrong: {wrongAnswer}";
-                answer.Text = "";
-                answer.Focus();
-            }
-
-        }
-
-        private void RandomNumber()
-        {
-
-            Random num = new Random();
-            firstNumber = num.Next(2, 10);
-            secondNumber = num.Next(2, 10);
-
-            question.Text = $"{firstNumber} " + "X" + $" {secondNumber}".ToString();
-        }
+        
     }
 }

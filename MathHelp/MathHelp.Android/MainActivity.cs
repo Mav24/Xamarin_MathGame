6ac44d1298e1883100aabdeb7bea0bd751650c04
Maverick <murrayduke@rogers.com>
2019-03-03 17:10:57 -0500

    Added Icons

--- MathHelp/MathHelp.Android/MainActivity.cs
+++ MathHelp/MathHelp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MathHelp.Droid
 {
-    [Activity(Label = "MathHelp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Math Help", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- MathHelp/MathHelp.Android/Properties/AssemblyInfo.cs
+++ MathHelp/MathHelp.Android/Properties/AssemblyInfo.cs
@@ -6,6 +6,7 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: Application(Icon = "@drawable/icon")]
 [assembly: AssemblyTitle("MathHelp.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
